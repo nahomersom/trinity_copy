f522123b67399ff2b5859a2249481410bf6e6c0e
nahom <nahomersom@outlook.com>
2024-04-02 08:19:29 +0300

    add hero slider

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,44 +1,110 @@
+'use client';
 import Image from "next/image";
 import Logo from "./ui/logo";
 import facebookIcon from "@/public/images/facebook_icon.svg";
 import instaIcon from "@/public/images/instagram_icon.svg";
 
+// export default function Hero() {
+//   return (
+//     <section className="relative h-[660px] max-h-full">
+//       {/* Illustration behind hero content */}
+//       <div
+//         className="absolute left-1/2 transform w-1/1 h-1/1 -translate-x-1/2 bottom-0 top-20 -z-1 overflow-hidden
+//         bg-[#002937] opacity-20
+//          pointer-events-none bg-cover bg-no-repeat  bg-center"
+//         aria-hidden="true"        
+//         style={{backgroundImage:"url('images/home_bg.svg')"}}
+//       >
+//       </div>
+
+     
+//         <div className="relative w-1/1 h-1/1 pt-32  md:pt-40 flex flex-col justify-center items-center">
+//           <div className="text-center pb-12 md:pb-16">
+//             <Logo size="big" />
+//           </div>
+          
+//       </div>
+//     </section>
+//   );
+// }
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore  from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/swiper-bundle.css'; // Ensure you import the Swiper bundle CSS file
+import {  Navigation, Autoplay } from 'swiper/modules';
+SwiperCore.use([Navigation]);
+
+SwiperCore.use([Autoplay]);
+
 export default function Hero() {
   return (
     <section className="relative h-[660px] max-h-full">
-      {/* Illustration behind hero content */}
-      <div
-        className="absolute left-1/2 transform w-1/1 h-1/1 -translate-x-1/2 bottom-0 top-20 -z-1 overflow-hidden
-        bg-[#002937] opacity-20
-         pointer-events-none bg-cover bg-no-repeat  bg-center"
-        aria-hidden="true"        
-        style={{backgroundImage:"url('images/home_bg.svg')"}}
-      >
-      </div>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={0}
+      autoplay={{ delay: 5000 }} // Autoplay with a delay of 5000 milliseconds (5 seconds)
+      navigation={false} // Hide navigation arrows
+      className="h-full"
+      loop={true}
+    >
+      <SwiperSlide>
+            <div
+       className="absolute left-1/2 transform w-1/1 h-1/1 -translate-x-1/2 bottom-0 top-20 -z-1 overflow-hidden
+         bg-[#002937] opacity-20
+          pointer-events-none bg-cover bg-no-repeat  bg-center"
+         aria-hidden="true"        
+         style={{backgroundImage:"url('images/home_bg.svg')"}}
+       >
+       </div>
 
-        {/* Hero content */}
-        <div className="relative w-1/1 h-1/1 pt-32  md:pt-40 flex flex-col justify-center items-center">
-          <div className="text-center pb-12 md:pb-16">
-            <Logo size="big" />
-          </div>
-          <div className="h-[57px] w-[28px] self-end bg-white flex flex-col mr-16 justify-evenly rounded-md">
-            <Image
-              className="md:max-w-none mx-auto rounded"
-              src={facebookIcon}
-              alt="Thrinity Fellowship"
-            />
-            <Image
-              className="md:max-w-none mx-auto rounded"
-              src={instaIcon}
-              alt="Thrinity Fellowship"
-            />
-          </div>
-          <div className=" h-12 w-56 self-start bg-white ml-10 absolute bottom-0">
+     
+         <div className="relative w-1/1 h-1/1 pt-32  md:pt-40 flex flex-col justify-center items-center">
+           <div className="text-center pb-12 md:pb-16">
+             <Logo size="big" />
+           </div>
+          
+       </div>
+      </SwiperSlide>
+      <SwiperSlide>
+            <div
+       className="absolute left-1/2 transform w-1/1 h-1/1 -translate-x-1/2 bottom-0 top-20 -z-1 overflow-hidden
+         bg-[#002937] opacity-20
+          pointer-events-none bg-cover bg-no-repeat  bg-center"
+         aria-hidden="true"        
+         style={{backgroundImage:"url('images/partners_pic.svg')"}}
+       >
+       </div>
 
-          </div>
+     
+         <div className="relative w-1/1 h-1/1 pt-32  md:pt-40 flex flex-col justify-center items-center">
+           <div className="text-center pb-12 md:pb-16">
+             <Logo size="big" />
+           </div>
+          
+       </div>
+      </SwiperSlide>
+      <SwiperSlide>
+            <div
+       className="absolute left-1/2 transform w-1/1 h-1/1 -translate-x-1/2 bottom-0 top-20 -z-1 overflow-hidden
+         bg-[#002937] opacity-20
+          pointer-events-none bg-cover bg-no-repeat  bg-center"
+         aria-hidden="true"        
+         style={{backgroundImage:"url('images/staff1.svg')"}}
+       >
+       </div>
 
-          {/* Hero image */}
-      </div>
+     
+         <div className="relative w-1/1 h-1/1 pt-32  md:pt-40 flex flex-col justify-center items-center">
+           <div className="text-center pb-12 md:pb-16">
+             <Logo size="big" />
+           </div>
+          
+       </div>
+      </SwiperSlide>
+    </Swiper>
     </section>
   );
 }
